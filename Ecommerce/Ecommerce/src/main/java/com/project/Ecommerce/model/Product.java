664af0853767828f3bb15be74fbd51d429661dc1
venkey3 <ponnapulavenkatesh546@gmail.com>
2025-05-04 18:03:20 +0530

    Fetch Product By Id changes

--- Ecommerce/Ecommerce/src/main/java/com/project/Ecommerce/model/Product.java
+++ Ecommerce/Ecommerce/src/main/java/com/project/Ecommerce/model/Product.java
@@ -3,8 +3,13 @@ package com.project.Ecommerce.model;
 import java.math.BigDecimal;
 import java.sql.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,16 +20,28 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Product {
 	
-	@Id
-	private int  id;
-	private String  name;
-	private String description;
-	private BigDecimal price;
-	private String category;
-	private Date releaseDate;
-	private boolean productAvailable;
-	private int stockQunatity;
 	
 	
+    public Product() {}
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
+
+    public Product(int id) {
+        this.id = id;
+    }
 }
